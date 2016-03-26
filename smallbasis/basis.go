1456c439786f4bc4b45ba708e0e1862b1f49ea0c
Alex Nichol <unixpickle@gmail.com>
2016-03-26 12:47:52 -0400

    created default basis based on DFT

---
+++ smallbasis/basis.go
@@ -0,0 +1,47 @@
+package smallbasis
+
+import (
+	"math"
+
+	"github.com/unixpickle/num-analysis/ludecomp"
+)
+
+// BasisMatrix generates a column matrix for
+// the standard image basis elements.
+func BasisMatrix(size int) *ludecomp.Matrix {
+	res := &ludecomp.Matrix{
+		N: size,
+		V: make([]float64, size*size),
+	}
+	for i := 0; i < size/2; i++ {
+		freq := 2 * math.Pi / float64(size)
+		for j := 0; j < size; j++ {
+			argument := float64(j)
+			res.Set(j, 2*i, math.Cos(argument*freq))
+			res.Set(j, 2*i+1, math.Sin(argument*freq))
+		}
+	}
+
+	// The last sin() should be replaced with cos(0*N) in every case,
+	// since the basis needs a vector of all 1's.
+	for i := 0; i < size; i++ {
+		res.Set(i, size-1, 1)
+	}
+
+	normalizeColumns(res)
+
+	return res
+}
+
+func normalizeColumns(m *ludecomp.Matrix) {
+	vec := make(ludecomp.Vector, m.N)
+	for col := 0; col < m.N; col++ {
+		for row := 0; row < m.N; row++ {
+			vec[row] = m.Get(row, col)
+		}
+		invMag := 1.0 / math.Sqrt(vec.Dot(vec))
+		for row := 0; row < m.N; row++ {
+			m.Set(row, col, vec[row]*invMag)
+		}
+	}
+}
